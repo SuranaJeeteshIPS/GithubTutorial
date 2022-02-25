8fcc9a677cef082fb91a145c10c51a50916fa9b8
Dev5 <dev5.itpathsolutions@gmail.com>
2022-02-25 18:51:05 +0530

    DONE: - [X] basic structure.
    WIP:
    - [] splash screen.

---
+++ basicStructure/shimmerEffect.kt
@@ -0,0 +1,16 @@
+1
+        2.
+        .323
+         .5sfs
+        sd
+        for ((i, e) in sdfsd.withIndex()) {
+
+        }fs
+        df
+        sdf
+        sd
+        sd
+
+        sdf
+        sdf
+        sdf
